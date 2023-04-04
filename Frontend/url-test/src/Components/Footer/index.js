30b401189d5af22e9ff1959bd6e54dc741bc47f2
4v <charviverma1117@gmail.com>
2023-04-04 22:42:15 +0530

    table

--- Frontend/url-test/src/Components/Footer/index.js
+++ Frontend/url-test/src/Components/Footer/index.js
@@ -1,7 +1,8 @@
 import "./style.scss";
 import React  from 'react';
 const table = () => {
-    
+    const blog = "huytduyqwgdukwg";
+    console.log(blog);
   return <div>
     
 </div>
--- Frontend/url-test/src/Components/Table/index.js
+++ Frontend/url-test/src/Components/Table/index.js
@@ -5,6 +5,35 @@ const table = () => {
   return <div id="theDiv">
     <div class="Results">
     <p class="heading">Last 10 Test Results</p>
+    <div class="tableHeader"></div>
+    <div>
+    <table class="table table-light ">
+  <thead>
+    <tr>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>Larry the Bird</td>
+      <td>twitter</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
     </div>
 </div>
 };
