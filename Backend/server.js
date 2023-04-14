b4b12324e0932359d752bc86e7d823d0ed5307e1
4v <charviverma1117@gmail.com>
2023-04-14 15:27:29 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  console.log("h");
+  console.log("connected");
 });
 
 exports.test = function(req,res) {
