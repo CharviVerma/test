28c8dafb19fdccc62181e7118cd13ef013de603f
4v <charviverma1117@gmail.com>
2023-04-14 11:13:55 +0530

    mongoose connect

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,17 @@
 const express= require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("h");
+});
+
+exports.test = function(req,res) {
+  res.render('test');
+};
 
 require('dotenv').config();
 
