3654a6dbf18703fc0f676b58c4838b1e77ed81a3
4v <charviverma1117@gmail.com>
2023-04-24 00:35:46 +0530

    err

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,17 @@ const bodyParser = require('body-parser');
 const UrlInterval = require('./models/UrlInterval');
 const UrlResponse = require('./models/UrlResponse');
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+
 const app = express();
+app.use(allowCrossDomain);
+
 const port = 3100;
 app.use(bodyParser.json());
 
