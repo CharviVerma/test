a23619be243e105a006478d174af7938c921f48e
4v <charviverma1117@gmail.com>
2023-04-24 14:10:17 +0530

    Footer

--- Frontend/url-test/src/Components/Footer/index.js
+++ Frontend/url-test/src/Components/Footer/index.js
@@ -1,7 +1,7 @@
 import "./style.scss";
 import React  from 'react';
 const table = () => {
-  return <div>
+  return <div id="footer_section">
     
 </div>
 };
--- Frontend/url-test/src/pages/Home/index.js
+++ Frontend/url-test/src/pages/Home/index.js
@@ -3,8 +3,8 @@ import React from "react";
 import Header from "../../Components/Header/index.js"
 import CheckUrl from "../../Components/CheckUrl/index.js"
 import DataTable from "../../Components/DataTable/index.js"
-import Footer from "../../Components/Footer/index.js"
 import DataTableUrl from "../../Components/DataTableUrl/index.js"
+import Footer from "../../Components/Footer/index.js"
 
 const Home = () => {
     return (
