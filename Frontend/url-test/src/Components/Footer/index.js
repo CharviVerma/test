1200a51228d5163a9de16a39699c41f661407d40
4v <charviverma1117@gmail.com>
2023-04-25 17:50:19 +0530

    Footer

--- Frontend/url-test/src/Components/Footer/index.js
+++ Frontend/url-test/src/Components/Footer/index.js
@@ -2,7 +2,15 @@ import "./style.scss";
 import React  from 'react';
 const table = () => {
   return <div id="footer_section">
-    
+    <div id="preFooter">
+    <div class="footerbottomleftside">
+      <p class="footersmalltext">© 2023 BrokenLinks. India All Rights Reserved</p>
+      <div class="termcondblock">
+        <a href="#" class="footerlink">privacy policy</a>
+        <a href="#" class="footerlink">Terms And conditions</a>
+        </div>
+        </div>
+    </div>
 </div>
 };
 
