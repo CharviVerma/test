234bd5b0b4955265a3075fbb62e89b265035a0c9
4v <charviverma1117@gmail.com>
2023-04-27 15:27:35 +0530

    .

--- Frontend/url-test/src/Components/Footer/index.js
+++ Frontend/url-test/src/Components/Footer/index.js
@@ -3,9 +3,20 @@ import React  from 'react';
 import logo from './first.png';
 const table = () => {
   return <div id="footer_section">
-    {/* <div id="footerMainRow"> */}
+    <div id="footerMainRow">
+      <div>
     <img src= {logo} alt='profile' className='profile'></img>
-    {/* </div> */}
+    <p>Lessssssgooooooooooooooooooooooo</p>
+    </div>
+    <div>
+    <img src= {logo} alt='profile' className='profile'></img>
+    <p>Lessssssgooooooooooooooooooooooo</p>
+    </div>
+    <div>
+    <img src= {logo} alt='profile' className='profile'></img>
+    <p>Lessssssgooooooooooooooooooooooo</p>
+    </div>
+    </div>
     <div id="preFooter">
     <div class="footerbottomleftside">
       <p class="footersmalltext">© 2023 BrokenLinks. India All Rights Reserved</p>
