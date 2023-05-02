4103e4fd34bb07a86f1f3f8a2b56e1c09c05cd45
4v <charviverma1117@gmail.com>
2023-05-02 10:51:41 +0530

    pages loginsignup

---
+++ Frontend/url-test/src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import "./style.scss";
+import React from "react";
+import Login from "../../Components/Login/index.js"
+import Signup from "../../Components/Signup/index.js"
+
+const LoginSignup = () => {
+    return (
+    <div>
+    <Login />
+    <Signup />
+</div>
+    )
+};
+
+export default LoginSignup;
