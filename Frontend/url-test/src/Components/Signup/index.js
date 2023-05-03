783875fc93b4c4ebe135b4f95acb17b49f5eec35
4v <charviverma1117@gmail.com>
2023-05-03 13:50:29 +0530

    .

--- Frontend/url-test/src/Components/Signup/index.js
+++ Frontend/url-test/src/Components/Signup/index.js
@@ -3,8 +3,20 @@ import React, { useState, useEffect } from "react";
 
 const Signup = () => {
 return(
-    <div>
-        
+    <div className="signin form">
+        <form action="session/" method="post">
+            <div className="form-fields">
+                <fieldset>
+                    <label for="login">Username or Email Address</label>
+                    <input/>
+                </fieldset>
+                <fieldset>
+                    <label></label>
+                    <input/>
+                </fieldset>
+            </div>
+
+        </form>
     </div>
 )
 }
