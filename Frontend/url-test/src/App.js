78f0f02e04296145500070f902000267cdeb2ed0
4v <charviverma1117@gmail.com>
2023-05-03 16:29:19 +0530

    .

--- Frontend/url-test/src/App.js
+++ Frontend/url-test/src/App.js
@@ -1,7 +1,11 @@
 import './App.scss';
 import {Routes,Route} from 'react-router-dom';
+
 import Home from "./pages/Home/index.js";
+import LoginSignup from "./pages/Login/index.js";
 import CheckUrl from "./Components/CheckUrl/index.js"
+import Login from "./Components/Login/index.js"
+import Signup from "./Components/Signup/index.js"
 
 
 
@@ -14,7 +18,11 @@ function App() {
         </Route>
         <Route path="/CheckUrl" element={<CheckUrl />}>
         </Route>
-        <Route path="/login" element={<CheckUrl />}>
+        <Route path="/LoginSignup" element={<LoginSignup/>}>
+        </Route>
+        <Route path="/Login" element={<Login/>}>
+        </Route>
+        <Route path="/Signup" element={<Signup/>}>
         </Route>
       </Routes>
   );
