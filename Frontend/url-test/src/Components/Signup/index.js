e7be4def1eddb16e5b413b50a9a3ee6266ce5225
4v <charviverma1117@gmail.com>
2023-05-05 14:55:06 +0530

    signup page

--- Frontend/url-test/src/Components/Signup/index.js
+++ Frontend/url-test/src/Components/Signup/index.js
@@ -3,26 +3,53 @@ import React, { useState, useEffect } from "react";
 import logo from './pretty.png';
 
 const Signup = () => {
+    // const form = document.getElementById('sign-in-form')
+    // form.addEventListener('submit', signInUser)
+    // async function signInUser(event){
+    //     event.preventDefault()
+    //     const username = document.getElementById('username').value
+    //     const password = document.getElementById('password').value
+
+    //     const result = await fetch('/api/signin', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             username,
+    //             password
+    //         })
+    //     }).then((res) => res.json())
+    //     console.log(result);
+    // }
 return(
-    <div>
-    <div className="signin form">
-        <form action="session/" method="post" className="form">
+    <div className="container">
+        <div className="row justify-content-center">
+    <div className="first form col-lg-9">
+        <div className="row box">
+            <div className="col-lg-6">
+        <form action="session/" method="post" id="sign-in-form">
             <div className="form-fields">
                 <fieldset>
                     <label for="login">Username or Email Address</label>
-                    <input type="text" classname="username" id="username"/>
+                    <input type="text" placeholder="username" autocomplete="off" classname="username" id="username"/>
                 </fieldset>
                 <fieldset>
                     <label for="password" className="password" id="password">Password</label>
-                    <input type="password" className="password" id="password"/>
+                    <input type="password" placeholder="password" autocomplete="off" className="password" id="password"/>
                 </fieldset>
             </div>
             <input type="submit" className="button form-sub" value="Sign In" data-cypress="submit-sign-in-btn"/>
 
         </form>
-        <div className="sidebar">
-        <img src= {logo} alt='profile' className='profile'></img>
         </div>
+        <div className="col-lg-6">
+        <div className="sidebar">
+        <img src= {logo} alt='profilee' className='profilee'></img>
+        </div>
+        </div>
+        </div>
+    </div>
     </div>
     </div>
 )
