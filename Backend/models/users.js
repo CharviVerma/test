e3b4e6bffc06c4abc721eb04ca2873006ab791ed
4v <charviverma1117@gmail.com>
2023-05-10 12:37:28 +0530

    fixed exports

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -8,6 +8,6 @@ const UserSchema = new mongoose.Schema(
 	{ collection: 'users' }
 )
 
-const model = mongoose.model('UserSchema', UserSchema)
+const user = mongoose.user('UserSchema', UserSchema)
 
-module.exports = model
+module.exports = user
