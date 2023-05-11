3edb8281b4750da5725fe7f19cb079f2e4a9a95e
4v <charviverma1117@gmail.com>
2023-05-11 13:28:46 +0530

    redirect page

--- Frontend/url-test/src/pages/Login/index.js
+++ Frontend/url-test/src/pages/Login/index.js
@@ -2,12 +2,14 @@ import "./style.scss";
 import React from "react";
 import Login from "../../Components/Login/index.js"
 import Signup from "../../Components/Signup/index.js"
+import DeepakAndYo from "../../Components/Deepak_and_Yo/index.js"
 
 const LoginSignup = () => {
     return (
     <div>
     <Login />
     <Signup />
+    <DeepakAndYo />
 </div>
     )
 };
