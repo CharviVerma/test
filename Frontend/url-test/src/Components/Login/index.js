573566fbb347afe87fdda4f503ad27d24a63cd3f
4v <charviverma1117@gmail.com>
2023-05-11 08:25:15 +0530

    .

--- Frontend/url-test/src/Components/Login/index.js
+++ Frontend/url-test/src/Components/Login/index.js
@@ -3,34 +3,7 @@ import "./style.scss";
 import logo from './pretty.png';
 
 const Login = () => {
-    const form = document.getElementById('login')
-			form.addEventListener('submit', login)
 
-			async function login(event) {
-				event.preventDefault()
-				const username = document.getElementById('username').value
-				const password = document.getElementById('password').value
-
-				const result = await fetch('/api/login', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
-						username,
-						password
-					})
-				}).then((res) => res.json())
-
-				if (result.status === 'ok') {
-					// everythign went fine
-					console.log('Got the token: ', result.data)
-					localStorage.setItem('token', result.data)
-					alert('Success')
-				} else {
-					alert(result.error)
-				}
-			}
 return(
     <div className="container">
         <div className="row justify-content-center">
--- Frontend/url-test/src/Components/Signup/index.js
+++ Frontend/url-test/src/Components/Signup/index.js
@@ -4,33 +4,7 @@ import logo from './pretty.png';
 
 const Signup = () => {
 
-
-    const form = document.getElementById('sign-in-form')
-    form.addEventListener('submit', registerUser)
-
-    async function registerUser(event) {
-        event.preventDefault()
-        const username = document.getElementById('username').value
-        const password = document.getElementById('password').value
-
-        const result = await fetch('/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        }).then((res) => res.json())
-
-        if (result.status === 'ok') {
-            // everythign went fine
-            alert('Success')
-        } else {
-            alert(result.error)
-        }
-    }
+    
 return(
     <div className="container">
         <div className="row justify-content-center">
