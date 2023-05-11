a9a5ff1ad869988532be2c124960cfa66228aa27
4v <charviverma1117@gmail.com>
2023-05-11 13:14:48 +0530

    .

---
+++ Frontend/url-test/src/Components/Deepak_and_Yo/index.js
@@ -0,0 +1,13 @@
+import "./style.scss";
+import React  from 'react';
+
+
+const redirectPage = () => {
+
+  return 
+  <div>
+    <p>Helloooooooooooooo</p>
+  </div>
+};
+
+export default redirectPage;
