5f844522c9b553211e770335efbdcae083f3634a
4v <charviverma1117@gmail.com>
2023-05-11 09:34:45 +0530

    .

--- Frontend/url-test/src/Components/Login/index.js
+++ Frontend/url-test/src/Components/Login/index.js
@@ -1,26 +1,49 @@
 import "./style.scss";
-// import React, { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import logo from './pretty.png';
 
 const Login = () => {
-
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const response = await axios.post('http://localhost:3000/login', { username, password });
+        const { token } = response.data;
+        localStorage.setItem('token', token); // Store the JWT token in local storage
+        setError('');
+        // Redirect to home page or dashboard
+      } catch (error) {
+        setError(error.response.data.message);
+      }
+    };
 return(
     <div className="container">
         <div className="row justify-content-center">
     <div className="first form col-lg-8">
         <div className="row box">
             <div className="col-lg-6">
-        <form action="session/" method="post" id="sign-in-form">
+        <form action="session/" method="post" id="sign-in-form" onSubmit={handleSubmit}>
             <div className="form-fields">
                 <fieldset>
                     <label for="Username">Username or Email Address</label>
+                    {error && <div>{error}</div>}
                     <input type="text" placeholder="Username" autocomplete="off" classname="username" id="username"
-                        
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        required
                     />
                 </fieldset>
                 <fieldset>
                     <label for="password" className="password" id="password">Password</label>
                     <input type="password" placeholder="Password" autocomplete="off" className="password" id="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    required
                     />
                 </fieldset>
             </div>
--- Frontend/url-test/src/Components/Signup/index.js
+++ Frontend/url-test/src/Components/Signup/index.js
@@ -3,7 +3,48 @@ import React, { useState } from "react";
 import logo from './pretty.png';
 
 const Signup = () => {
-
+    function Signup() {
+        const [username, setUsername] = useState('');
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+        const [confirmPassword, setConfirmPassword] = useState('');
+      
+        const handleUsernameChange = (event) => {
+          setUsername(event.target.value);
+        };
+      
+        const handleEmailChange = (event) => {
+          setEmail(event.target.value);
+        };
+      
+        const handlePasswordChange = (event) => {
+          setPassword(event.target.value);
+        };
+      
+        const handleConfirmPasswordChange = (event) => {
+          setConfirmPassword(event.target.value);
+        };
+      
+        const handleSubmit = async (event) => {
+          event.preventDefault();
+      
+          if (password !== confirmPassword) {
+            alert('Passwords do not match.');
+            return;
+          }
+      
+          try {
+            const response = await axios.post('http://localhost:3000/signup', {
+              username,
+              email,
+              password,
+            });
+            console.log(response.data);
+            // Redirect to home page or login page
+          } catch (error) {
+            console.log(error);
+          }
+        };
     
 return(
     <div className="container">
@@ -39,6 +80,38 @@ return(
     </div>
     </div>
 )
+}
+
+export default Signup;
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <label>
+          Confirm Password:
+          <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
